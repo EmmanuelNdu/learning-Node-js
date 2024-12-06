21a13429714057171e262462ce0be571572d53a4
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-06 16:14:55 +0100

    first commit

---
+++ postController.js
@@ -0,0 +1,10 @@
+const posts = [
+    {id: 1, title: 'post One'},
+    {id: 2, title: 'post Two'}
+];
+
+ const getPosts = () => posts;
+
+ export const getPostsLength = () => posts.length;
+
+export default getPosts;
