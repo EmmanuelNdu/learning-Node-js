8cbd5fce29fd5eb84de26a560b1b1f0b5f53578c
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-07 19:48:21 +0100

    try catch

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 import http from 'http'
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/html');
     // res.statusCode = 404;
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end('<h1>Hello World!</h1>')
+    // console.log(req.url)
+    // console.log(req.method)
+
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end('<h1>Homepage</h1>');
+    } else if (req.url === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end('<h1>About</h1>');
+    } else {
+        res.writeHead(400, {'Content-Type': 'text/html'})
+        res.end('<h1>Not Found</h1>')
+    }
+   
 });
 
 server.listen(PORT, () => {
