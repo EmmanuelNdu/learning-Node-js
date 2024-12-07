adf20dcd5a81c53351d7d297945749fecd35a87e
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-07 20:10:03 +0100

    public

--- server.js
+++ server.js
@@ -6,17 +6,30 @@ const server = http.createServer((req, res) => {
     // res.statusCode = 404;
     // console.log(req.url)
     // console.log(req.method)
+    try {
+        // check if Get request
+        if (req.method === 'GET') {
 
-    if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.end('<h1>Homepage</h1>');
-    } else if (req.url === '/about') {
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.end('<h1>About</h1>');
-    } else {
-        res.writeHead(400, {'Content-Type': 'text/html'})
-        res.end('<h1>Not Found</h1>')
+            if (req.url === '/') {
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.end('<h1>Homepage</h1>');
+            } else if (req.url === '/about') {
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.end('<h1>About</h1>');
+            } else {
+                res.writeHead(400, {'Content-Type': 'text/html'})
+                res.end('<h1>Not Found</h1>')
+            }
+
+
+        } else {
+            throw new Error ('Method not allowed')
+        }
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/plain'})
+                res.end('<h1>Server Error</h1>')
     }
+
    
 });
 
