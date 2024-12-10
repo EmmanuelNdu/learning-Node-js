9895b0f8b2e5f4e02c2b5c8e625943da19573c65
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-10 23:38:06 +0100

    terminal

--- fsDemo.js
+++ fsDemo.js
@@ -12,6 +12,41 @@ import fs from 'fs/promises'
 // console.log(data);
 
 // readFile() - Promise .then()
-fs.readFile('./test.txt', 'utf8')
-.then((data) => console.log(data))
-.catch((err) => console.log(err));
+// fs.readFile('./test.txt', 'utf8')
+// .then((data) => console.log(data))
+// .catch((err) => console.log(err));
+
+// readfile () - async/await
+const readFile = async () => {
+    try {
+        const data =await fs.readFile('./test.txt', 'utf8');
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//writeFile()
+
+const writeFile = async () => {
+    try {
+        await fs.writeFile('./test.txt', 'Hello, i am writing to this file');
+        console.log('file written to...')
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// appendFile()
+const appendFile = async () => {
+    try {
+        await fs.appendFile('./test.txt', '\nThis is appended text');
+        console.log('File appended to...');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+writeFile();
+appendFile();
+readFile();
