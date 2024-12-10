94f8e77f24ad8dad9f8e2569b8f4621776dd1372
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-10 12:48:58 +0100

    try

--- server2.js
+++ server2.js
@@ -7,10 +7,30 @@ const users = [
     {id: 2, name: 'Jim Doe'}
 ];
 
+// Logger middleware
+const logger = ( req, res, next ) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
 const server = createServer ((req, res) => { 
     if (req.url === '/api/users' && req.method === 'GET' ) {
         res.setHeader ('Content-Type', 'application/json');
         res.write(JSON.stringify(users));
+        res.end();
+    } else if  (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
+        const user = users.find((user) => user.id === parseInt(id) );
+        res.setHeader ('Content-Type', 'application/json');
+        if  ( user) {
+            res.write(JSON.stringify(user));
+        } else {
+        res.statusCode = 404;
+        res.write(JSON.stringify({message: 'User not found'}));
+        }
+     } else  {
+        res.setHeader ('Content-Type', 'application/json');
+        res.statusCode= 404;
+        res.write(JSON.stringify({message: 'Route not found'}));
         res.end();
      }
 })
